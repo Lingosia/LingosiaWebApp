6c9ffc1f363bab6ab91adcad8b0e8f9d9ae1b6c0
JonathanMint <jonathan.tester@googlemail.com>
2025-06-15 14:09:58 +0100

    Redirect HTTP and WWW

--- Server/index.js
+++ Server/index.js
@@ -24,3 +24,25 @@ require('./word')(app);
 https.createServer(options, app).listen(port, () => {
     console.log(`API server running at https://localhost:${port}`);
 });
+
+// Redirect HTTP to HTTPS
+const http = require('http');
+const httpApp = express();
+httpApp.use(function(req, res) {
+    res.redirect('https://' + req.headers.host + req.originalUrl);
+});
+
+http.createServer(httpApp).listen(80, () => {
+    console.log('HTTP server running on port 80, redirecting to HTTPS');
+});
+
+// Redirect www to non-www
+app.use((req, res, next) => {
+    if (req.headers.host.startsWith('www.')) {
+        const newHost = req.headers.host.replace('www.', '');
+        res.redirect(301, `https://${newHost}${req.originalUrl}`);
+    }
+    else {
+        next();
+    }
+});
